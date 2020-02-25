812e877d08dace1a8fe262f60a96910ccd540871
dbc <cy991587100@163.com>
2020-02-25 20:29:23 +0800

    first commit

---
+++ learn_tool_test/src/main/java/com/dbc/learn_tool_test/LearnToolTestApplication.java
@@ -0,0 +1,13 @@
+package com.dbc.learn_tool_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnToolTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearnToolTestApplication.class, args);
+    }
+
+}
---
+++ learn_tool_test/src/test/java/com/dbc/learn_tool_test/LearnToolTestApplicationTests.java
@@ -0,0 +1,14 @@
+package com.dbc.learn_tool_test;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class LearnToolTestApplicationTests {
+
+    @Test
+    public void firstTest() {
+        System.out.println("first test");
+    }
+
+}
